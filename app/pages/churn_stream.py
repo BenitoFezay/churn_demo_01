07263a3ad4a83e5aae803b3478945b5970723baf
Benito Fezay <152497587+BenitoFezay@users.noreply.github.com>
2024-11-12 11:43:08 +0300

    Update churn_stream.py

--- app/pages/churn_stream.py
+++ app/pages/churn_stream.py
@@ -7,6 +7,14 @@ import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 
+# STREMLIT APP
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+# PICKEL
+import pickle
 
 # ---------------------------------
 # 1- ENOCDING "Churn"
@@ -15,7 +23,7 @@ label_encoder = LabelEncoder()
 
 
 # -------------------------------
-# 6- Scalling data with StandardScaler
+# 2- Scalling data with StandardScaler
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
@@ -28,54 +36,49 @@ def making_scaler_standardscaler(df):
     df[columns] = scaler.fit_transform(df[columns])
 
 # --------------------------------
-# 7- SPLITTING THE DATA
-# 7.1- RESAMPLING 
+# 3- SPLITTING THE DATA
+# 3.1- RESAMPLING 
 from imblearn.over_sampling import SMOTE
 smote = SMOTE()
 
-# 7.2- Using train and test split
+# 3.2- Using train and test split
 from sklearn.model_selection import train_test_split
 
 # -------------------------------
-# 8- CREATE MODEL
+# 4- CREATE MODEL
 from sklearn.linear_model import LogisticRegression
 
 # -------------------------------
-# 10- ACCURACY
+# 5- ACCURACY
 import sklearn.metrics as sm
 
 # ------------------------------
-# 11- CONFUSION-MATRIX
+# 6- CONFUSION-MATRIX
 from sklearn.metrics import confusion_matrix as cm
 
 # ------------------------------
-# 12- CLASSIFICATION_REPORT
+# 7- CLASSIFICATION_REPORT
 from sklearn.metrics import classification_report as cr
 
 # ------------------------------
-# 13- CROSS VALIDATION
+# 8- CROSS VALIDATION
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 
 # ------------------------------
-# 14- BOOSTING MODEL
+# 9- BOOSTING MODEL
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 
-
 # -------------------------------
-# 17- SAVING MODEL
-import pickle
-
+# 10- SAVING MODEL
 model_path = './model/churn_model.pkl'
-# pickle.dump(model_xgb, open(model_path, 'wb'))
 
 # To load the model later:
-# churn_model = joblib.load('./model/churn_model.pkl')
 churn_model = pickle.load(open(model_path, 'rb'))
 
 
 # -------------------------------
-# 18- TESTING THE MODEL
+# 11- TESTING THE MODEL
 def testing_model_by_ilocation(data):
     # new_data = X_test.iloc[data]  # Example: Use the first row of X_test for testing
     prediction = churn_model.predict(data)
@@ -90,16 +93,10 @@ def testing_model_by_ilocation(data):
 
 
 # ------------------------------
-# 19- STREMLIT APP
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import math
-
 st.title("🤖 Machine Learning")
 
 # --------------------------------
-# 21- Churn prediction BY FILL FIELDS
+# 12- Churn prediction BY FILL FIELDS
 def make_data_encoded(data):
     # ['TypeCompte', 'MontantTrans', 'TypeTransaction', 'ScoreCSAT',
     #    'ScoreNPS', 'AgeCompte (j)', 'AgeClient', 'Ville', 'MontantPret',
@@ -136,98 +133,95 @@ def make_data_encoded(data):
         if data["Ville"] == ville:
             data["Ville"] = i
 
-
-
+# 13- Fields for churn prediction
 with st.expander("CHURN PREDICTION - BY FILLING FIELDS"):
-    st.write("#### Input Data")
-    with st.form(key="Churn_form"):
+       
+       st.write("#### Input Data")
+       with st.form(key="Churn_form"):
         
-        col1, col2, col3 = st.columns(3)
-        # home_value = col1.number_input("Data location", min_value=0, max_value=len(X_test), value=0)
-        # montant_transaction
-        transaction_amount = col1.number_input("Montant Transaction (ar)", min_value=2_000, value=450_000)
-        # Score_csat
-        score_csat = col1.slider('Score CSAT client', 0, 10, 3)
-        # Montant pret
-        loan_amount = col1.number_input("Montant pret", min_value=0, value=0)
-        # taux d'interet
-        taux_interet = col2.slider("Taux d'interet", 0.00, 5.00, 3.12)
-        # age compte 
-        age_compte = col2.number_input("Age compte (j)", min_value=3, value=2300)
-        # scorre nps
-        score_nps = col2.slider('Score NPS client', 0, 10, 3)
-        # type compte
-        type_account = col3.selectbox('Type de Compte', ('Compte Courant', 'Compte Épargne'))
-        # type engagement
-        type_engagement = col3.selectbox("Type d'engagement", ('Utilisation application mobile', 'Participation de programme de fidelité'))
-        # type transaction
-        type_transaction = col3.selectbox("Type de Transaction", ('Virement', 'Paiement' ,'Retrait', 'Depôt'))
-        # age client
-        age_client = col3.slider('Age client', 18, 100, 47)
-        # ville
-        ville = col2.selectbox("Adresse", ('Antsohihy', 'Ambanja', 'Antananarivo', 'Ihosy', 'Toliara',
-        'Antsiranana', 'Fianarantsoa', 'Toamasina', 'Fort Dauphin',
-        'Mahajanga', 'Andapa', 'Ambovombe', 'Morondava', 'Nosy Be'))
-        # button submit
-        submit_button = st.form_submit_button(label="Valide & predict") 
-        
-
-    data = {
-        "TypeCompte": type_account,
-        'MontantTrans': transaction_amount, 
-        'TypeTransaction': type_transaction, 
-        'ScoreCSAT': score_csat,
-        'ScoreNPS': score_nps, 
-        'AgeCompte (j)': age_compte, 
-        'AgeClient': age_client, 
-        'Ville': ville, 
-        'MontantPret': loan_amount,
-        'TauxInteret': taux_interet, 
-        'TypeEngagement': type_engagement
-    }
-        
-    make_data_encoded(data=data)
-
-    if submit_button:
-
-        df = pd.DataFrame(data, index=[0])
-
-        # Scalling input data
-        from sklearn.preprocessing import StandardScaler
-        columns = {'MontantTrans': {"mean":0.015519 , "std": 0.554258}, 
-                'ScoreCSAT': {"mean":-0.101133 ,"std":0.572905}, 
-                'ScoreNPS': {"mean":0.054224, "std":0.567020}, 
-                'AgeCompte (j)': {"mean":-0.002555 , "std":0.584303}, 
-                'AgeClient':{"mean":4.693210, "std":3.337875}, 
-                'MontantPret': {"mean":0.502908, "std":0.572779}, 
-                'TauxInteret': {"mean": 0.495658, "std":0.499983}}
-
-        # with st.expander('Input features'):
-        st.write('**Input data before scalling**')
-        input_df = df.copy()
-        input_df
+              col1, col2, col3 = st.columns(3)
+              # montant_transaction
+              transaction_amount = col1.number_input("Montant Transaction (ar)", min_value=2_000, value=450_000)
+              # Score_csat
+              score_csat = col1.slider('Score CSAT client', 0, 10, 3)
+              # Montant pret
+              loan_amount = col1.number_input("Montant pret", min_value=0, value=0)
+              # taux d'interet
+              taux_interet = col2.slider("Taux d'interet", 0.00, 5.00, 3.12)
+              # age compte 
+              age_compte = col2.number_input("Age compte (j)", min_value=3, value=2300)
+              # scorre nps
+              score_nps = col2.slider('Score NPS client', 0, 10, 3)
+              # type compte
+              type_account = col3.selectbox('Type de Compte', ('Compte Courant', 'Compte Épargne'))
+              # type engagement
+              type_engagement = col3.selectbox("Type d'engagement", ('Utilisation application mobile', 'Participation de programme de fidelité'))
+              # type transaction
+              type_transaction = col3.selectbox("Type de Transaction", ('Virement', 'Paiement' ,'Retrait', 'Depôt'))
+              # age client
+              age_client = col3.slider('Age client', 18, 100, 47)
+              # ville
+              ville = col2.selectbox("Adresse", ('Antsohihy', 'Ambanja', 'Antananarivo', 'Ihosy', 'Toliara',
+               'Antsiranana', 'Fianarantsoa', 'Toamasina', 'Fort Dauphin',
+               'Mahajanga', 'Andapa', 'Ambovombe', 'Morondava', 'Nosy Be'))
+              # button submit
+              submit_button = st.form_submit_button(label="Valide & predict") 
         
 
-
-        # scaler = StandardScaler()
-        data = np.array(input_df)
-        # input_df[columns] = scaler.fit_transform(input_df[columns])
-
-        def manual_standardize(df, columns):
-            for col, stats in columns.items():
-                mean = stats["mean"]
-                std = stats["std"]
-                # Standardiser la colonne en utilisant la formule
-                df[col] = (data - mean) / std
-            return df
-
-        manual_standardize(input_df, columns=columns)
-
-        # input_penguins = pd.concat([input_df, X_test], axis=0)
-        st.write('**Input data after scalling**')
-        input_df
-
-        testing_model_by_ilocation(data=input_df)
+              data = {
+               "TypeCompte": type_account,
+               'MontantTrans': transaction_amount, 
+               'TypeTransaction': type_transaction, 
+               'ScoreCSAT': score_csat,
+               'ScoreNPS': score_nps, 
+               'AgeCompte (j)': age_compte, 
+               'AgeClient': age_client, 
+               'Ville': ville, 
+               'MontantPret': loan_amount,
+               'TauxInteret': taux_interet, 
+               'TypeEngagement': type_engagement
+              }
+              
+              make_data_encoded(data=data)
+              
+              if submit_button:
+               df = pd.DataFrame(data, index=[0])
+               # Scalling input data
+               from sklearn.preprocessing import StandardScaler
+               columns = {'MontantTrans': {"mean":0.015519 , "std": 0.554258}, 
+                       'ScoreCSAT': {"mean":-0.101133 ,"std":0.572905}, 
+                       'ScoreNPS': {"mean":0.054224, "std":0.567020}, 
+                       'AgeCompte (j)': {"mean":-0.002555 , "std":0.584303}, 
+                       'AgeClient':{"mean":4.693210, "std":3.337875}, 
+                       'MontantPret': {"mean":0.502908, "std":0.572779}, 
+                       'TauxInteret': {"mean": 0.495658, "std":0.499983}}
+              
+               # with st.expander('Input features'):
+               st.write('**Input data before scalling**')
+               input_df = df.copy()
+               input_df
+               
+              
+              
+               # scaler = StandardScaler()
+               data = np.array(input_df)
+               # input_df[columns] = scaler.fit_transform(input_df[columns])
+              
+               def manual_standardize(df, columns):
+                   for col, stats in columns.items():
+                       mean = stats["mean"]
+                       std = stats["std"]
+                       # Standardiser la colonne en utilisant la formule
+                       df[col] = (data - mean) / std
+                   return df
+              
+               manual_standardize(input_df, columns=columns)
+              
+               # input_penguins = pd.concat([input_df, X_test], axis=0)
+               st.write('**Input data after scalling**')
+               input_df
+              
+               testing_model_by_ilocation(data=input_df)
 
 
 # --------------------------------
