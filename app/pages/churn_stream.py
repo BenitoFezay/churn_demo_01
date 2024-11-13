b8e41282ce89f2bc50324734f8781accf673ada9
Benito Fezay <152497587+BenitoFezay@users.noreply.github.com>
2024-11-13 13:52:48 +0300

    Update churn_stream.py

--- app/pages/churn_stream.py
+++ app/pages/churn_stream.py
@@ -295,6 +295,7 @@ def prepare_data(df_uploaded):
                      df_churn = make_encoding_labelencoder(df_churn, columns_to_encoded)
                      # Scaling the data using standardscaler
                      df_churn = making_scaler_standardscaler(df_churn)
+                     df_churn = df_churn[0:100]
                      # make prediction
                      making_prediction(df_churn)
     
