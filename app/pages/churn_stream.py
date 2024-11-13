ed0fe861f80474cbdbbc47bda6b179bd01a247f8
Benito Fezay <152497587+BenitoFezay@users.noreply.github.com>
2024-11-13 12:43:21 +0300

    Update churn_stream.py

--- app/pages/churn_stream.py
+++ app/pages/churn_stream.py
@@ -283,7 +283,8 @@ def churn_prediction_by_uploading_file(df_uploaded):
                             df_pred['Classification'] = df_pred['Prediction'].map({1: 'Churner', 0: 'Loyal'})
                             # Count the occurrences of each classification
                             classification_counts = df_pred['Classification'].value_counts()
-                            st.write(type(classification_counts))
+                            classification_counts = pd.DataFrame(classification_counts)
+                            st.write(classification_counts.columns)
                             # st.bar_chart(classification_counts, x="Classification", y="count")
                      
                      except ValueError as e:
